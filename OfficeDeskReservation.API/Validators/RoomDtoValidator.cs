785296536b0d7f8ff8d8fa07da8a09b50cc97927
Roman <168230669+dramonRog@users.noreply.github.com>
2026-03-07 14:00:41 +0100

    fix: remove unuseful dependency in RoomDtoValidator

--- OfficeDeskReservation.API/Validators/RoomDtoValidator.cs
+++ OfficeDeskReservation.API/Validators/RoomDtoValidator.cs
@@ -1,17 +1,12 @@
 ﻿using FluentValidation;
-using Microsoft.EntityFrameworkCore;
-using OfficeDeskReservation.API.Data;
 using OfficeDeskReservation.API.Dtos.Rooms;
 
 namespace OfficeDeskReservation.API.Validators
 {
     public class RoomDtoValidator : AbstractValidator<RoomDto>
     {
-        private readonly AppDbContext _context;
-        public RoomDtoValidator(AppDbContext context)
+        public RoomDtoValidator()
         {
-            _context = context;
-
             RuleFor(r => r.Name)
                 .NotEmpty().WithMessage("Room name can't be empty!")
                 .Length(3, 30).WithMessage("Room name must have at least 3 characters and no more than 30");
